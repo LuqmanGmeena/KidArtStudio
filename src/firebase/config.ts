4669b92f8a57ab550553d25e788230d9341649d4
El Manoh <Luqmanmeena1@gmail.com>
2025-09-13 01:38:47 +0300

    Updated config.ts

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -17,12 +17,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Exports
-export const db = getFirestore(app);
-export const auth = getAuth(app);
-export const storage = getStorage(app);
-export default app;
-
 // Initialize Firebase services
 export const auth = getAuth(app);
 export const db = getFirestore(app);
